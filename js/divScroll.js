611f020f750ffeebaeb868e159c0a18237e31fcd
rbarocio <rodrigobarocio@gmail.com>
2019-11-21 16:55:35 -0800

    Final Update
    Updated Website, Added Additional pages per owner request. Increased font Size for easier legibility. Added shadows to make text stand out more.

--- js/divScroll.js
+++ js/divScroll.js
@@ -1,58 +1,58 @@
-let ScrollRate = 100;
-
-function scrollDiv_init() {
-  try{
-	DivElmnt = document.getElementById('yelp-reviews');
-	ReachedMaxScroll = false;
-
-	DivElmnt.scrollTop = 0;
-	PreviousScrollTop  = 0;
-
-	ScrollInterval = setInterval('scrollDiv()', ScrollRate);
-}catch(e){
-    if(e){
-    // If fails, Do something else
-    }
-}
-}
-
-function scrollDiv() {
-try{
-	if (!ReachedMaxScroll) {
-		DivElmnt.scrollTop = PreviousScrollTop;
-		PreviousScrollTop++;
-
-		ReachedMaxScroll = DivElmnt.scrollTop >= (DivElmnt.scrollHeight - DivElmnt.offsetHeight);
-	}
-	else {
-		ReachedMaxScroll = (DivElmnt.scrollTop == 0)?false:true;
-
-		DivElmnt.scrollTop = PreviousScrollTop;
-		PreviousScrollTop--;
-	}
-}catch(e){
-    if(e){
-    // If fails, Do something else
-    }
-}
-}
-function pauseDiv() {
-  try{
-	clearInterval(ScrollInterval);
-}catch(e){
-    if(e){
-    // If fails, Do something else
-    }
-}
-}
-
-function resumeDiv() {
-  try{
-	PreviousScrollTop = DivElmnt.scrollTop;
-	ScrollInterval    = setInterval('scrollDiv()', ScrollRate);
-}catch(e){
-    if(e){
-    console.log("Error");
-  }
-}
-}
+let ScrollRate = 100;
+
+function scrollDiv_init() {
+  try{
+	DivElmnt = document.getElementById('yelp-reviews');
+	ReachedMaxScroll = false;
+
+	DivElmnt.scrollTop = 0;
+	PreviousScrollTop  = 0;
+
+	ScrollInterval = setInterval('scrollDiv()', ScrollRate);
+}catch(e){
+    if(e){
+    // If fails, Do something else
+    }
+}
+}
+
+function scrollDiv() {
+try{
+	if (!ReachedMaxScroll) {
+		DivElmnt.scrollTop = PreviousScrollTop;
+		PreviousScrollTop++;
+
+		ReachedMaxScroll = DivElmnt.scrollTop >= (DivElmnt.scrollHeight - DivElmnt.offsetHeight);
+	}
+	else {
+		ReachedMaxScroll = (DivElmnt.scrollTop == 0)?false:true;
+
+		DivElmnt.scrollTop = PreviousScrollTop;
+		PreviousScrollTop--;
+	}
+}catch(e){
+    if(e){
+    // If fails, Do something else
+    }
+}
+}
+function pauseDiv() {
+  try{
+	clearInterval(ScrollInterval);
+}catch(e){
+    if(e){
+    // If fails, Do something else
+    }
+}
+}
+
+function resumeDiv() {
+  try{
+	PreviousScrollTop = DivElmnt.scrollTop;
+	ScrollInterval    = setInterval('scrollDiv()', ScrollRate);
+}catch(e){
+    if(e){
+    console.log("Error");
+  }
+}
+}
